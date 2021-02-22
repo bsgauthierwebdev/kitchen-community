47c036cb7b189db9ce08e1d04224a29e9d4b063e
Brent Gauthier <brentsgauthier@gmail.com>
2021-02-21 21:03:54 -0500

    Edited RecipeListMain.js

--- src/RecipeListMain/RecipeListMain.js
+++ src/RecipeListMain/RecipeListMain.js
@@ -1,20 +1,46 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import Recipe from '../Recipe/Recipe';
+import ApiContext from '../ApiContext'
+import {getRecipesForFolder} from '../recipes-helpers'
 
 export default class RecipeListMain extends React.Component {
+
+    static defaultProps = {
+        match: {
+            params: {}
+        }
+    }
+    static contextType = ApiContext
+
     render() {
+        const {folderId} = this.props.match.params
+        const {recipes = []} = this.context
+        const recipesForFolder = getRecipesForFolder(recipes, folderId)
+
         return (
             <section className = 'RecipeListMain'>
                 <ul>
-                    <li>This list</li>
-                    <li>will be popluated</li>
-                    <li>by the recipes</li>
-                    <li>submitted by users.</li>
-                    <li>Each item in this list</li>
-                    <li>will be a link</li>
-                    <li>to the individual recipe page.</li>
+                    {recipesForFolder.map(recipe =>
+                        <li key = {NodeIterator.id}>
+                            <Recipe
+                                id = {recipe.id}
+                                name = {recipe.name}
+                                modified = {recipe.modified}
+                            />
+                        </li>
+                    )}
                 </ul>
+                <div className = 'RecipeListMain__button-container'>
+                <button
+                    tag = {Link}
+                    to = '/add-note'
+                    type = 'button'
+                    className = 'RecipeListMain__add-recipe-button'
+                >
+                    Add Recipe
+                </button>
+                </div>
             </section>
         )
     }
