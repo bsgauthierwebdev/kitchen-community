717a2b0fc53754a0a9dc6a3da39a59d169346005
Brent Gauthier <brentsgauthier@gmail.com>
2021-02-24 19:18:38 -0500

    Added headers for recipe items

--- src/RecipePageMain/RecipePageMain.js
+++ src/RecipePageMain/RecipePageMain.js
@@ -30,31 +30,37 @@ export default class RecipePageMain extends React.Component {
                     history = {this.props.history}
                 />
                 <div className = 'RecipePageMain__description'>
+                    <h4>Description:</h4> 
                     {recipe.description.split(/\n \r|\n/).map((para, i) =>
                         <p key = {i}>{para}</p>
                         )}
                 </div>
                 <div className = 'RecipePageMain__prepTime'>
+                    <h4>Prep Time: </h4> 
                     {recipe.prepTime.split(/\n \r|\n/).map((para, i) =>
                         <p key = {i}>{para}</p>
                         )}
                 </div>
                 <div className = 'RecipePageMain__cookTime'>
+                    <h4>Cook Time:</h4>
                     {recipe.cookTime.split(/\n \r|\n/).map((para, i) =>
                         <p key = {i}>{para}</p>
                         )}
                 </div>
                 <div className = 'RecipePageMain__servings'>
+                    <h4>Servings:</h4> 
                     {recipe.servings.split(/\n \r|\n/).map((para, i) =>
                         <p key = {i}>{para}</p>
                         )}
                 </div>
                 <div className = 'RecipePageMain__ingredients'>
+                    <h4>Ingredients:</h4> 
                     {recipe.ingredients.split(/\n \r|\n/).map((para, i) =>
                         <p key = {i}>{para}</p>
                         )}
                 </div>
                 <div className = 'RecipePageMain__directions'>
+                    <h4>Directions:</h4> 
                     {recipe.directions.split(/\n \r|\n/).map((para, i) =>
                         <p key = {i}>{para}</p>
                         )}
